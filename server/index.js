a65bca38d1fb8d06ebeccd8e25e89756c8f7d883
davervrcic <daver.vrcic@gmail.com>
2020-10-26 10:53:06 +0100

    setup backend

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-1
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=> {
+    res.send("h100000eloooooo1o");
+})
+
+app.listen(3001, () => {
+    console.log("runnig on port 3001");
+})
