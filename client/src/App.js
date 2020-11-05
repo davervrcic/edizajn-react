99d7b00399afbdd42f695a5cde954b91f3df78c1
davervrcic <daver.vrcic@gmail.com>
2020-11-05 21:47:16 +0100

    major change 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,135 @@
+import React, {useState, useEffect, Component} from "react";
 import './App.css';
+import Axios from "axios";
+
 
 function App() {
+
+  class imageUpload extends Component {
+
+    state = {
+      file: null
+    }
+
+    
+    handleFile(e){
+
+      let file = e.target.files[0]
+     
+      this.setState({file: file});
+
+      }
+      
+    handleUpload(e){
+      console.log(this.state, "THE STATE ----------- $$$$");
+    }
+
+  }
+
+  const [projectName, setProjectName] = useState();
+  const [projectYear, setProjectYear] = useState();
+  const [projectUrl, setProjectUrl] = useState();
+  const [projectType, setProjectType] = useState();
+  const [projectImage, setProjectImage] = useState();
+  const [projectsList, setProjectsList] = useState([]);
+
+  /**const [newProjectName, setNewProject] = useState("");
+  const [newProjectYear, setNewYear] = useState("");
+  const [newProjectUrl, setNewUrl] = useState("");
+  const [newProjectType, setNewType] = useState("");
+  const [newProjectImage, setNewImage] = useState("");**/
+
+   useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response)=> {
+      setProjectsList(response.data)
+    });
+  }, []);
+
+  const submitProject = () => {
+    Axios.post("http://localhost:3001/api/insert", 
+            {projectName: projectName, projectYear: projectYear, projectUrl: projectUrl, projectType: projectType, projectImage: projectImage});
+
+            setProjectsList([...projectsList, {projectName: projectName, projectYear: projectYear, projectUrl: projectUrl, projectType: projectType, projectImage: projectImage},]);
+  };
+
+  const deleteProject = (project) => {
+    Axios.delete(`http://localhost:3001/api/delete/${project}`);
+  };
+
+  /**const updateProject = (project) => {
+    Axios.put("http://localhost:3001/api/update", 
+    {projectName: project, projectYear: newProjectYear, projectUrl: newProjectUrl, projectType: newProjectType, projectImage: newProjectImage});
+
+    setNewProject("");
+  };**/
+
+  
+
+
   return (
+
+    
     <div className="App">
-      <h1>CRUD APPLICATION</h1>
+      <h1>ADD PROJECT</h1>
       <div className="form">
         <label>Project Name:</label>
-        <input type="text" name="projectName"></input>
+        <input type="text" name="projectName" onChange={(e)=> {
+          setProjectName(e.target.value)
+
+        }}></input>
 
         <label>Project Year:</label>
-        <input type="text" name="projectYear"></input>
+        <input type="text" name="projectYear" onChange={(e)=> {
+          setProjectYear(e.target.value)
+
+        }}></input>
 
         <label>Project Url:</label>
-        <input type="text" name="projectUrl"></input>
+        <input type="text" name="projectUrl" onChange={(e)=> {
+          setProjectUrl(e.target.value)
+
+        }}></input>
 
         <label>Project Type:</label>
-        <input type="text" name="projectType"></input>
+        <input type="text" name="projectType" onChange={(e)=> {
+          setProjectType(e.target.value)
+
+        }}></input>
 
         <label>Project Image:</label>
-        <input type="text" name="projectImage"></input>
+        <input type="text" name="projectImage" onChange={(e)=> {
+          setProjectImage(e.target.value)
 
-        <button>Submit</button>
+        }}></input>
+
+        <input type="file" name="file" onChange={(e) => {imageUpload.handleFile(e)}}/>
+
+        <button type="button" onClick={(e) => {imageUpload.handleUpload(e)}}>Upload</button>
+
+
+
+        <button onClick={submitProject}>Submit</button>
+
+        {projectsList.map((val)=> {
+          return (
+            <div className="card">            
+              <h1>{val.projectName}</h1>
+              <p>{val.projectYear}</p>
+              <p>{val.projectType}</p>
+              <p>{val.projectUrl}</p>
+              <p>{val.projectImage}</p>   
+
+              <button onClick={() => {deleteProject(val.projectName)}}>Delete</button>
+
+
+              <input type="text" id="updateInput" /**onChange={(e) => {
+                  setNewProject(e.target.value)}}**/></input>
+
+              <button /**onClick={() => {updateProject(val.projectName)}}**/>Update</button>     
+            </div>
+
+          )
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,9 +1,70 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
 
-app.get('/', (req,res)=> {
-    res.send("h100000eloooooo1o");
-})
+const db = mysql.createPool({
+    host: "localhost",
+    user: "daver",
+    password:"Infodax123***",
+    database: "projectsdb",
+
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req,res)=> {
+
+    const sqlSelect = "SELECT * FROM projects";
+   db.query(sqlSelect, (err, result)=> {
+       res.send(result);
+   });
+    
+    });
+    
+
+app.post("/api/insert", (req,res)=> {
+
+    const projectName = req.body.projectName;
+    const projectYear = req.body.projectYear;
+    const projectType = req.body.projectType;
+    const projectUrl = req.body.projectUrl;
+    const projectImage = req.body.projectImage;
+
+   const sqlInsert = "INSERT INTO projects (projectName, projectYear, projectType, projecUrl, projectImage ) VALUES (?,?,?,?,?)";
+   db.query(sqlInsert, [projectName, projectYear, projectType, projectUrl, projectImage], (err, result)=> {
+       console.log(err);
+   });
+    
+    });
+
+app.delete("/api/delete/:projectName", (req,res)=> {
+        const name = req.params.projectName;
+        const sqlDelete = "DELETE FROM projects WHERE projectName = ?";
+
+        db.query(sqlDelete, name, (err, result) => {
+           if (err) console.log(err);
+        });
+
+    });
+
+    /**app.put("/api/update", (req,res)=> {
+        const name = req.body.projectName;
+        const year = req.body.projectYear;
+        const type = req.body.projectType;
+        const url = req.body.projectUrl;
+        const image = req.body.projectImage;
+        const sqlUpdate = "UPDATE projects SET projectName = ? WHERE projectName = ?";
+
+        db.query(sqlUpdate, [name, year, type, url, image], (err, result) => {
+           if (err) console.log(err);
+        });
+
+    });**/
+    
 
 app.listen(3001, () => {
     console.log("runnig on port 3001");
